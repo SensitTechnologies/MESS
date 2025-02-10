f54a6ee03f9a270f1f7d76a1174d22f08a3c83e0
xyclose11 <harrisgus1@hotmail.com>
2025-02-10 14:41:42 -0600

    AuditableEntity + Validator

---
+++ MESS/MESS.Data/Models/AuditableEntity.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+
+namespace MESS.Data.Models;
+
+public abstract class AuditableEntity
+{
+    public required string CreatedBy { get; set; }
+    public required DateTimeOffset CreatedOn { get; set; }
+    public required string LastModifiedBy { get; set; }
+    public required DateTimeOffset LastModifiedOn { get; set; }
+}
+
+public class AuditableEntityValidator : AbstractValidator<AuditableEntity>
+{
+    public AuditableEntityValidator()
+    {
+        RuleFor(x => x.CreatedBy)
+            .NotNull()
+            .NotEmpty()
+            .Length(1, 512)
+            .WithMessage("Created By character count must be between 1 and 512 characters.");
+        
+        RuleFor(x => x.CreatedOn)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.LastModifiedBy)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.LastModifiedOn)
+            .NotNull()
+            .GreaterThanOrEqualTo(x => x.CreatedOn)
+            .WithMessage("Last Modified On must be after or equal to Created On.")
+            .LessThanOrEqualTo(DateTimeOffset.UtcNow)
+            .WithMessage("Last Modified On must be before or equal to the current UTC time.");
+        
+    }
+}
