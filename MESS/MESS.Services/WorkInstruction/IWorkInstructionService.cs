58f1159de386cecaa11f601d26433859b52ec880
xyclose11 <harrisgus1@hotmail.com>
2025-02-15 13:53:28 -0600

    Basic WorkInstruction View

--- MESS/MESS.Services/WorkInstruction/IWorkInstructionService.cs
+++ MESS/MESS.Services/WorkInstruction/IWorkInstructionService.cs
@@ -1,6 +1,9 @@
 namespace MESS.Services.WorkInstruction;
+using Data.Models;
 
 public interface IWorkInstructionService
 {
-    
+    public List<WorkInstruction> GetAll();
+    public WorkInstruction? GetByTitle(string title);
+    public WorkInstruction? GetById(int id);
 }
--- MESS/MESS.Services/WorkInstruction/WorkInstructionService.cs
+++ MESS/MESS.Services/WorkInstruction/WorkInstructionService.cs
@@ -1,6 +1,51 @@
-namespace MESS.Services.WorkInstruction;
+using MESS.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
+namespace MESS.Services.WorkInstruction;
+using MESS.Data.Models;
 public class WorkInstructionService : IWorkInstructionService
 {
-    
+    private readonly ApplicationContext _context;
+    public WorkInstructionService(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public List<WorkInstruction> GetAll()
+    {
+        try
+        {
+            var workInstructions = _context.WorkInstructions
+                .Include(w => w.Steps)
+                .ToList();
+
+            return workInstructions;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public WorkInstruction? GetByTitle(string title)
+    {
+        try
+        {
+            var workInstruction = _context.WorkInstructions
+                .FirstOrDefault(w => w.Title == title);
+
+            return workInstruction;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+
+    public Data.Models.WorkInstruction? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
