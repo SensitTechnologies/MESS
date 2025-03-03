0c2a0095db2072f1e9b23a88d7218017f8bbceca
xyclose11 <harrisgus1@hotmail.com>
2025-03-03 16:12:11 -0600

    WorkStationSelect Testing

---
+++ MESS/MESS.Tests/UI Testing/ProductionLog/WorkStationSelectTests.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using MESS.Blazor.Components.Pages.ProductionLog;
+using MESS.Data.Models;
+
+namespace MESS.Tests.UI_Testing.ProductionLog;
+
+public class WorkStationSelectTests : TestContext
+{
+    [Fact]
+    public void WorkStationSelectComponentRendersWith1Selection()
+    {
+        // Act
+        var cut = RenderComponent<WorkStationSelect>(parameters => parameters
+            .Add(p => p.WorkStations, new List<WorkStation>()));
+
+        // Assert
+        var selectElement = cut.Find("select#product-select");
+        Assert.Equal(1, selectElement.Children.Length);
+    }
+}
