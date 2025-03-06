571e39f84d1a8e94373a8b711f496aeefa7cec25
xyclose11 <harrisgus1@hotmail.com>
2025-03-06 14:49:13 -0600

    Made productionLogStep Success nullable

--- MESS/MESS.Data/DTO/ProductionLogStepDTO.cs
+++ MESS/MESS.Data/DTO/ProductionLogStepDTO.cs
@@ -4,7 +4,7 @@ public class ProductionLogStepDTO
 {
     public int WorkInstructionStepId { get; set; }
     public int ProductionLogId { get; set; }
-    public bool Success { get; set; }
+    public bool? Success { get; set; }
     public DateTimeOffset SubmitTime { get; set; }
     public bool ShowNotes { get; set; }
     public string? Notes { get; set; }
