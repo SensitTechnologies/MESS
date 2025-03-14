95369a66a9223a3f3dc7890826b0931434826caa
Sam Thyen <Eye_Brawler@outlook.com>
2025-03-13 19:24:57 -0500

    Adding Missing Generic Type Parameters

--- MESS/MESS.Data/Context/ApplicationContext.cs
+++ MESS/MESS.Data/Context/ApplicationContext.cs
@@ -1,10 +1,11 @@
 using MESS.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MESS.Data.Context;
 
-public class ApplicationContext : IdentityDbContext<LineOperator>
+public class ApplicationContext : IdentityDbContext<LineOperator, IdentityRole, string>
 {
     public ApplicationContext()
     {
