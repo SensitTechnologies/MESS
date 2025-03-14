b5bf821d3cfe5f61f3ffb5cac5626fc3b020fbf7
Sam Thyen <Eye_Brawler@outlook.com>
2025-03-14 12:58:45 -0500

    Making Separate Identity DbContext

---
+++ MESS/MESS.Data/Context/UserContext.cs
@@ -0,0 +1,13 @@
+using MESS.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MESS.Data.Context;
+
+public class UserContext : IdentityDbContext<LineOperator, IdentityRole, string>
+{
+    public UserContext(DbContextOptions<UserContext> options) : base(options)
+    {
+    }
+}
