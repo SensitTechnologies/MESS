736bde288b870d0cb1f832f1a8d93178333508a5
Harris G <harris.gustafson@valpo.edu>
2025-04-17 12:45:15 -0500

    Auth Test Setup

--- MESS/MESS.Tests/UI_Testing/ProductionLog/EndToEnd/ProductionLogCreationE2ETests.cs
+++ MESS/MESS.Tests/UI_Testing/ProductionLog/EndToEnd/ProductionLogCreationE2ETests.cs
@@ -5,6 +5,19 @@ namespace MESS.Tests.UI_Testing.ProductionLog.EndToEnd;
 public class ProductionLogCreationE2ETests : PageTest
 {
     [Fact]
+    public async Task Login_PersistAuthState_Successfully()
+    {
+        var page = await Page.Context.NewPageAsync();
+
+        await page.GotoAsync("https://localhost:7152");
+        await page.GetByText("").SelectOptionAsync("TechnicianUser");
+        await Page.Context.StorageStateAsync(new()
+        {
+            Path = "../../../playwright/.auth/state.json"
+        });
+    }
+    
+    [Fact]
     public async Task CreateProductionLog_AsOperator_SuccessfullyCreatesProductionLog()
     {
         await Page.GotoAsync("https://localhost:7152");
