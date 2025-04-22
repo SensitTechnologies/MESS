0ef9ac7be76c08933c3bc2201e59db8d839b7924
Harris G <harris.gustafson@valpo.edu>
2025-04-22 12:16:09 -0500

    SQLite DB Test Setup

--- MESS/MESS.Tests/Services/ProductionLogServiceTests.cs
+++ MESS/MESS.Tests/Services/ProductionLogServiceTests.cs
@@ -1,6 +1,7 @@
 using MESS.Data.Context;
 using MESS.Services.ProductionLog;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Moq;
 
 namespace MESS.Tests.Services.ProductionLog;
@@ -9,11 +10,37 @@ using Data.Models;
 public class ProductionLogServiceTests
 {
     private readonly ProductionLogService _productionLogService;
-    private readonly Mock<IDbContextFactory<ApplicationContext>> _mockDbContextFactory;
 
     public ProductionLogServiceTests()
     {
-        _mockDbContextFactory = new Mock<IDbContextFactory<ApplicationContext>>();
-        _productionLogService = new ProductionLogService(_mockDbContextFactory.Object);
+        // Configure DbContextOptions to use SQLite with a file-based database
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseSqlite("Data Source=TestDatabase.db")
+            .Options;
+
+        // Ensure the database is created before tests run
+        using (var context = new ApplicationContext(options))
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        // Set up the DbContextFactory to return a properly configured ApplicationContext instance
+        var dbFactory = new Mock<IDbContextFactory<ApplicationContext>>();
+        dbFactory.Setup(f => 
+                f.CreateDbContextAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ApplicationContext(options));
+
+        _productionLogService = new ProductionLogService(dbFactory.Object);
+    }
+
+    [Fact]
+    public async Task CreateLog_EmptyLog_SuccessfullyAppliesDefaultValues()
+    {
+        var productionLogToAdd = new ProductionLog();
+
+        var productionLogToAddId = await _productionLogService.CreateAsync(productionLogToAdd);
+        
+        Assert.Equal(1,productionLogToAddId);
     }
 }
