9e4dfb87fb5cc6cf10c83297f9d50f214b97c7ca
xyclose11 <harrisgus1@hotmail.com>
2025-04-30 16:19:29 -0500

    Work Instruction Import ignores "Black" on import to enable dark/light mode for those cells

--- MESS/MESS.Services/WorkInstruction/WorkInstructionService.cs
+++ MESS/MESS.Services/WorkInstruction/WorkInstructionService.cs
@@ -673,8 +673,11 @@ public partial class WorkInstructionService : IWorkInstructionService
                         {
                             var themeColor = workbook.Theme.ResolveThemeColor(richText.FontColor.ThemeColor);
                             var color = themeColor.Color;
-                            string hexColor = $"#{color.R:X2}{color.G:X2}{color.B:X2}";
-                            styles.Add($"color: {hexColor}");
+                            if (!string.Equals(color.Name, "Black"))
+                            {
+                                string hexColor = $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+                                styles.Add($"color: {hexColor}");
+                            }
                         }
                     }
                     
