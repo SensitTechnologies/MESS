e86950b6e36d62900feb86c541f444dd45da7de5
xyclose11 <harrisgus1@hotmail.com>
2025-04-30 19:39:27 -0500

    Adjusted scroll logic

--- MESS/MESS.Blazor/Components/Pages/Phoebe/WorkInstruction/WorkInstructionNodeManagerList.razor.js
+++ MESS/MESS.Blazor/Components/Pages/Phoebe/WorkInstruction/WorkInstructionNodeManagerList.razor.js
@@ -1,5 +1,5 @@
-const SCROLL_SPEED = 30;
-const SCROLL_THRESHOLD = 350;
+const SCROLL_SPEED = 50;
+const SCROLL_THRESHOLD = 300;
 
 class DragScroller {
     constructor() {
@@ -9,33 +9,23 @@ class DragScroller {
     }
     
     registerEvents() {
-        document.addEventListener('dragover', this.handleDragOver.bind(this));
-        document.addEventListener('dragleave', this.resetScroll.bind(this));
+        document.addEventListener('drag', this.handleDragOver.bind(this));
         document.addEventListener('drop', this.resetScroll.bind(this));
-    }
-
-    smoothScroll(){
-        if (this.scrollSpeed !== 0) {
-            window.scrollBy(0, this.scrollSpeed);
-            this.animationFrame = requestAnimationFrame(this.smoothScroll);
-        }
     }
 
     handleDragOver(e) {
         const isNearTopEdge = e.clientY < SCROLL_THRESHOLD;
         const isNearBottomEdge = (window.innerHeight - e.clientY) < SCROLL_THRESHOLD;
-
+        
         if (isNearTopEdge) {
             this.scrollSpeed = -SCROLL_SPEED;
         } else if (isNearBottomEdge) {
             this.scrollSpeed = SCROLL_SPEED;
-        } else {
-            this.scrollSpeed = 0;
         }
-
-        if (!this.animationFrame) {
-            this.animationFrame = requestAnimationFrame(this.smoothScroll.bind(this));
-        }
+        window.scrollBy({
+            top: this.scrollSpeed,
+            behavior: 'smooth',
+        });
     }
 
     resetScroll() {
