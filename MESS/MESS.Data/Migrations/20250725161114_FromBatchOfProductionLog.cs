7ebafb3df02399d6f717c74de4ccc41ee40fe18c
Sam Thyen <sam.thyen@valpo.edu>
2025-07-25 11:23:40 -0500

    Database Migration for FromBatchOf Production Log Field

---
+++ MESS/MESS.Data/Migrations/20250725161114_FromBatchOfProductionLog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MESS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FromBatchOfProductionLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FromBatchOf",
+                table: "ProductionLogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromBatchOf",
+                table: "ProductionLogs");
+        }
+    }
+}
